9500662c25a006b2b6fff4a71868e08492a7012f
Vineet Kumar <vineet@wavemarket.com>
2007-10-16 15:57:26 -0700

    simplify morris further
    by using concatMap and group instead of span and cons-recursion

--- morris.hs
+++ morris.hs
@@ -1,9 +1,8 @@
 -- e.g.: ["1", "11", "21", "1211", "111221", "312211", "13112221",
 -- "1113213211", "31131211131221", "13211311123113112211"]
+import Data.List
 
 morris = iterate nextMorris "1"
 
 nextMorris :: String -> String
-nextMorris [] = ""
-nextMorris s@(c:cs) = let (a,b) = span (== c) s in
-	show (length a) ++ c:nextMorris(b)
+nextMorris = concatMap (\a -> show (length a) ++ [head a]) . group
