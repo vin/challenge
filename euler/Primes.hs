a1b68ccd52a930e3a85875b43e9ac6941bca4928
Vineet Kumar <vineet@doorstop.net>
2007-11-28 15:00:25 -0800

    use a faster numfactors function. With this, it's possible to use a simpler, less hacky triangle number generator, and it still runs in a reasonable amount of time.

--- euler/Primes.hs
+++ euler/Primes.hs
@@ -1,4 +1,5 @@
-module Primes (isPrime, primes, factor) where
+module Primes (isPrime, primes, factor, numfactors) where
+import Data.List (group)
 
 isPrime a = isPrimeHelper a primes
 
@@ -12,3 +13,5 @@ primes = 2 : filter isPrime [3,5..]
 factor 1 = []
 factor n = let f = head (filter (\p -> n `mod` p == 0) primes) in
     f: factor (n `div` f)
+
+numfactors n = product $ map ((+1).length) $ (group.factor) n
--- euler/euler012.hs
+++ euler/euler012.hs
@@ -1,15 +1,6 @@
 import Data.List
-import Primes
+import Primes (numfactors)
 
-tri n = n * (n+1) `div` 2
-tris = map tri [2500..]
+tris = scanl1 (+) [1..]
 
---factors = ((map (factors' 1) [0..]) !!)
-factors = factors' 1
-  where
-    factors' k n | k*k > n        = []
-		 | k*k == n       = [k]
-		 | n `mod` k == 0 = k : n `div` k : factors' (k+1) n
-		 | otherwise      = factors' (k+1) n
-
-main = print $ find ((>= 500) . length . factors) tris
+main = print $ find ((>= 500) . numfactors) tris
