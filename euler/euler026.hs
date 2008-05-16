877d83e622ab4371bdf7f0aceee676937b501fb5
Vineet Kumar <vineet@doorstop.net>
2008-05-16 15:50:21 -0700

    euler026: add some explanatory comments, clean up cycle_length function.

--- euler/euler026.hs
+++ euler/euler026.hs
@@ -1,7 +1,14 @@
 import Primes
 import Control.Arrow
 
-cycle_length n = length $ 10 : (takeWhile (not . (==10)) . map (\k -> 10^k `mod` n)) [2..]
+{- Wikipedia: Recurring Decimal:
+-- The period of the repeating decimal of 1⁄p is equal to the order of 10
+-- modulo p. The period is equal to p-1 if 10 is a primitive root modulo p.
+- We just use our list of primes, so we can ignore multiples of 10.
+- "order of 10 modulo p" means the smallest k st 10^k = 1 (mod p)
+-}
+
+cycle_length p = head $ dropWhile (\k -> 10^k `mod` p /= 1) [1..]
 
 main = putStrLn . show $ snd . maximum $
             map (cycle_length &&& id) (takeWhile (< 1000) (drop 10 primes))
