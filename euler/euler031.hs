dd88131e45eacb00622de2197435604648880c8f
Vineet Kumar <vineet@doorstop.net>
2008-05-16 09:21:35 -0700

    solve euler 31

--- euler/euler031.hs
+++ euler/euler031.hs
@@ -1,19 +1,19 @@
--- denominations = [ 1,2,5,10,20,50,100,200 ]
 denominations = [ 200, 100, 50, 20, 10, 5, 2, 1]
 
 change :: Integer -> [Integer] -> [[Integer]]
 change 1 _ = [[1]]
-change _ [] = []
-change 0 _ = []
-change n denominations = 
-	concatMap (\d -> map (\x -> d:x) (change (n-d) denominations) ++ change d denominations) (filter (<=n) denominations)
-
+change 0 _ = [[]]
 {--
+ -ex: change 3 [2,1] = [2:change 1 [2,1], 1:change 2 [1]]
+ -    change 4 [2,1] = [2:change 2 [2,1], 1:change 3 [1]]
+ -    change 5 [5,2,1] = [5:change 0 [5,2,1], 2:change 3 [2,1], 1:change 4 [1]]
+ -    change 6 [5,2,1] = [5:change 1 [5,2,1], 2:change 4 [2,1], 1:change 5 [1]]
+ -  but since change gives [[Integer]], when I say "5:change 1 [5,2,1]", that
+ -  really means "map (5:) (change 1 [5,2,1])", and we use concatMap to string
+ -  them together.
+ -}
+change n denominations = let ds = (filter (<=n) denominations) in
+    concatMap (\d -> map (d:) $ change (n-d) (filter (<=d) ds)) ds
 
-change 1 = [[1]]
-change 2 = [[2],[1,1]]
-change 3 = [[2,1],[1,1,1]]
-change 4 = [[2,2],[2,1,1],[1,1,1,1]]
-change 5 = [[5],[2,2,1],[2,1,1,1],[1,1,1,1,1]]
 
-}
+main = print $ length $ change 200 denominations
