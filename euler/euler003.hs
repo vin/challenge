12cd83795c61f9396c933580aaccfc3af4a7b930
Vineet Kumar <vineet@doorstop.net>
2008-05-15 21:58:46 -0700

    euler003: add main, use Primes module.

--- euler/euler003.hs
+++ euler/euler003.hs
@@ -1,14 +1,5 @@
-num = 317584931803
+import Primes
 
-isPrime a = isPrimeHelper a primes
+num = 600851475143
 
-isPrimeHelper a (p:ps)
-        | p*p > a = True
-        | a `mod` p == 0 = False
-        | otherwise = isPrimeHelper a ps
-
-primes = 2 : filter isPrime [3..]
-
-factor 1 = []
-factor n = let f = head (filter (\p -> n `mod` p == 0) primes) in
-	f: factor (n `div` f)
+main = print $ last $ factor num
