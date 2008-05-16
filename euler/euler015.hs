380e951b689ddf8cb2d58b061a86e1bec90a068a
Vineet Kumar <vineet@doorstop.net>
2008-05-15 22:32:30 -0700

    haskell implementation of euler problem 19.

--- euler/euler015.hs
+++ euler/euler015.hs
@@ -1,2 +1,4 @@
 fac n = product [1..n]
 comb n k = fac n `div` (fac k) `div` (fac (n-k))
+
+main = print $ comb 40 20
--- euler/euler019.hs
+++ euler/euler019.hs
@@ -1,3 +1,8 @@
-import System.Time
+import Data.Time.Calendar
+import Data.Time.Calendar.WeekDate
 
-start = addToClockTime (TimeDiff (-68) 0 0 0 0 0 0) (TOD 0 0)
+sundays = filter (\(_,_,dow) -> (dow == 7)) . map toWeekDate
+
+firsts = [ fromGregorian year month 1 | year <- [1901..2000],
+                                        month <- [1..12]]
+main = print $ length $ sundays firsts
