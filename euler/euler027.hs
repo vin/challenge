c98e1e251b51b96e9665c6307262392cee804b50
Vineet Kumar <vineet@doorstop.net>
2009-01-05 01:11:27 -0500

    euler027: generalize the strict maximumBy function.

--- euler/euler027.hs
+++ euler/euler027.hs
@@ -1,8 +1,16 @@
 import Control.Arrow
 import Primes
+import Data.List(foldl1', maximumBy)
+import Data.Ord(comparing)
 
 primeLength f = length . takeWhile isPrime $ map f [0..]
 
 quad a b x = x*x + a*x + b
 
-main = print . snd . maximum $ map (primeLength . uncurry quad &&& id) [(a,b) | a <- [-999..999], b <- [-999..999]]
+
+maximumByComparing' :: (Ord a, Ord b) => (a -> b) -> [a] -> a
+maximumByComparing' cmp = snd . maximum . map (cmp &&& id)
+
+main = print $ maximumByComparing' (primeLength . uncurry quad) [(a,b) | b <- takeWhile(<1000) primes, a <- [(-b)..b]]
+
+--main = print . snd . maximum $ map (primeLength . uncurry quad &&& id) [(a,b) | b <- takeWhile (<1000) primes, a <- [(-b)..b]]
