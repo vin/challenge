af7c55a1b1236686decd6666ab70817592e693bd
Vineet Kumar <vineet@doorstop.net>
2009-01-05 17:40:03 -0500

    euler049: add comments.
    I'm not sure how much it really helps, but it's better than nothing.

--- euler/euler049.hs
+++ euler/euler049.hs
@@ -9,7 +9,7 @@ permutations (x:xs)     =  [zs | ys <- permutations xs, zs <- interleave x ys ]
         interleave x []     =  [[x]]
         interleave x (y:ys) =  [x:y:ys] ++ map (y:) (interleave x ys)
 
-
+-- these are all the 4-digit primes
 candidates = takeWhile (<9999) $ dropWhile (<1000) $ primes
 
 differences (a:[]) = []
@@ -17,7 +17,9 @@ differences (a:b:cs) = (b-a):differences (b:cs)
 
 candidates2:: [Integer]
 -- This is a beast:
--- first get all the 4-digit permutations of each 4-digit prime.
+-- first get all the 4-digit permutations of each 4-digit prime (map read.filter (not.isPrefixOf "0").nub.permutations.show)
+-- Then filter out just the prime permutations and sort those. (sort.filter isPrime)
+-- And pull out only those whose differences form an arithmetic progression (any (>1).map length.group.differences)
 candidates2 = filter (any (>1).map length.group.differences.sort.filter isPrime.map read.filter (not.isPrefixOf "0").nub.permutations.show) candidates
 
 
