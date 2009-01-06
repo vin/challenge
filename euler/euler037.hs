d30586cbe1c72ac61db77377aa8e125a4dc70df8
Vineet Kumar <vineet@doorstop.net>
2009-01-05 21:56:06 -0500

    Use Primes module for euler037.
    This must also have been written before Primes was a reusable module.

--- euler/euler037.hs
+++ euler/euler037.hs
@@ -1,15 +1,6 @@
+import Primes
 
-isPrime 1 = False
-isPrime a = isPrimeHelper a primes
-
-isPrimeHelper a (p:ps)
-        | p*p > a = True
-        | a `mod` p == 0 = False
-        | otherwise = isPrimeHelper a ps
-
-primes = 2 : filter isPrime [3..]
-
-isTruncatablePrime :: Int -> Bool
+isTruncatablePrime :: Integer -> Bool
 isTruncatablePrime n | n == 0 = True
 			| n < 10 = False
 			| otherwise = isPrime n &&
