e6c3721901727fb309ab237ffb257d785f4faf0e
Vineet Kumar <vineet@doorstop.net>
2009-01-11 01:22:19 -0500

    solve euler098, albeit a bit ugly.
    At least it's fast, though.

--- euler/euler098.hs
+++ euler/euler098.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Ord
+import Data.Maybe
 import Control.Arrow
 
 eqing :: (Eq b) => (a -> b) -> a -> a -> Bool
@@ -9,6 +10,8 @@ sortedEq = eqing sort
 
 fstEq :: (Eq a) => (a,b) -> (a,b) -> Bool
 fstEq = eqing fst
+sndEq :: (Eq b) => (a,b) -> (a,b) -> Bool
+sndEq = eqing snd
 
 squares = map (\x -> x*x) $ [1..]
 
@@ -28,9 +31,19 @@ stringMap :: Int -> [String] -> [String]
 stringMap n (s:ss) = let numstring = show n in
 -}
 
+-- rearrange a string according to a permutation list
+-- e.g. permute "ABCD" [0,1,3,2] -> ABDC
+permute _ [] = ""
+permute cs (n:ns) = (cs!!n) : permute cs ns
 
+-- e.g. unpermute "ABCD" "ABDC" -> [0,1,3,2]
+unpermute _ "" = []
+unpermute from (c:cs) = (fromJust . elemIndex c $ from) : unpermute from cs
 
-uniqueMap s = all (null.drop 1.group) . groupBy fstEq . sort . zip s . show
+isSquare n = (==n) . head . dropWhile (<n) $ squares
+
+uniqueMap s n = let mapping = nub . sort . zip s . show $ n in
+  null . drop 1 . group . map (\f -> f mapping) $ [nubBy fstEq, nubBy sndEq, nub]
 {-
 - e.g.:
 -  > filter (uniqueMap "foo") $ 3 `digit` squares
@@ -42,8 +55,17 @@ uniqueMap s = all (null.drop 1.group) . groupBy fstEq . sort . zip s . show
 candidatesquares s = filter (uniqueMap s) $ n `digit` squares
   where n = length s
 
+success :: String -> String -> Integer -> Bool
+success a b n = let permutation =  permute (show n) $ unpermute a b in
+  (not . (== '0') . head $ permutation) && 
+    (isSquare . read $ permutation)
+
+squareAnagrams a b = filter (success a b) . candidatesquares $ a
+
 main = do
   f <- readFile "words.txt"
   let ws = lines f
-  print . map (map (length.candidatesquares)) . anagrams $ ws
+  let as = reverse . sortBy (comparing $ map length) . anagrams $ ws
+  let candidates = filter (not.null) . map (\(a:b:cs) -> max (squareAnagrams a b) (squareAnagrams b a)) $ as
+  print . head $ candidates
 
